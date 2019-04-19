47dfaea642aae5aaf9549f325901a257b91fddad
JohnCoene <jcoenep@gmail.com>
2019-04-19 12:54:32 +0200

    layout

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ ui <- fluidPage(
   use_waypointer(),
   div(
     id = "stick",
-    sigmajsOutput("graph")
+    sigmajsOutput("graph", width = "100%", height = "100vh")
   ),
   longdiv(
     wp(1)
@@ -50,17 +50,15 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
 
-   w1 <- Waypoint$
-    new(
-      "1"
-    )$
-    start()
-
-   w2 <- Waypoint$
-    new(
-      "2"
-    )$
-    start()
+   w1 <- Waypoint$new("1")$start()
+   w2 <- Waypoint$new("2")$start()
+   w3 <- Waypoint$new("3")$start()
+   w4 <- Waypoint$new("4")$start()
+   w5 <- Waypoint$new("5")$start()
+   w6 <- Waypoint$new("6")$start()
+   w7 <- Waypoint$new("7")$start()
+   w8 <- Waypoint$new("8")$start()
+   w9 <- Waypoint$new("9")$start()
 
   # Our sticky plot
   shtick <- Shtick$
@@ -68,16 +66,46 @@ server <- function(input, output, session) {
     shtick()
 
   output$graph <- renderSigmajs({
-    sigmajs()
+    sigmajs() 
   })
 
   observeEvent(w1$get_direction(), {
-    if(w1$get_direction() == "down")
-      add_data(1)
+    if(w1$get_direction() == "down") add_data(1)
   })
 
   observeEvent(w2$get_direction(), {
+    if(w2$get_direction() == "down") add_data(2)
+  })
 
+  observeEvent(w3$get_direction(), {
+    if(w3$get_direction() == "down") add_data(3)
+  })
+
+  observeEvent(w4$get_direction(), {
+    if(w4$get_direction() == "down") add_data(4)
+  })
+
+  observeEvent(w5$get_direction(), {
+    if(w5$get_direction() == "down") add_data(5)
+  })
+
+  observeEvent(w6$get_direction(), {
+    if(w6$get_direction() == "down") add_data(6)
+  })
+
+  observeEvent(w7$get_direction(), {
+    if(w7$get_direction() == "down") add_data(7)
+  })
+
+  observeEvent(w8$get_direction(), {
+    if(w8$get_direction() == "down") add_data(8)
+  })
+
+  observeEvent(w9$get_direction(), {
+    if(w9$get_direction() == "down") add_data(9)
+    Sys.sleep(3)
+    sigmajsProxy("graph") %>% 
+      sg_force_stop_p()
   })
   
 }
--- functions.R
+++ functions.R
@@ -20,7 +20,25 @@ add_data <- function(wp){
     filter(appear == wp)
 
   sigmajsProxy("graph") %>% 
+    sg_force_kill_p() %>% 
     sg_read_nodes_p(n, id, label, color, size) %>% 
     sg_read_edges_p(e, id, source, target, weight) %>% 
-    sg_read_exec_p()
+    sg_read_exec_p() %>% 
+    sg_refresh_p() %>% 
+    sg_force_start_p(scalingRatio = 40, slowDown = 5, strongGravityMode = TRUE)
+}
+
+drop_data <- function(wp){
+  n <- nodes %>% 
+    filter(appear == wp)
+  
+  e <- edges %>% 
+    filter(appear == wp)
+
+  sigmajsProxy("graph") %>%
+    sg_force_kill_p() %>%  
+    sg_drop_edges_p(e, id, refresh = FALSE) %>% 
+    sg_drop_nodes_p(n, id, refresh = FALSE) %>% 
+    sg_refresh_p() %>% 
+    sg_force_start_p(scalingRatio = 40, slowDown = 5)
 }
